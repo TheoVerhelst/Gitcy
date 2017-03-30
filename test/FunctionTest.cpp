7721ca7a46cca16d8aaf27194719f83fa7e7a05a
TheoVerhelst <tverhels@ulb.ac.be>
2017-03-30 15:46:48 +0200

    Add test for Function

---
+++ test/FunctionTest.cpp
@@ -0,0 +1,78 @@
+#include <boost/test/unit_test.hpp>
+#include <ScriptError.hpp>
+#include <Function.hpp>
+#include <Overload.hpp>
+
+class OneInt : public Overload
+{
+	public:
+		OneInt():
+			Overload{{SignatureType::create<int>()}, false}
+		{
+		}
+
+		virtual Data operator()(const std::vector<Data>&) const override
+		{
+			return "blah";
+		}
+};
+
+class VariadicDouble : public Overload
+{
+	public:
+		VariadicDouble():
+			Overload{{SignatureType::create<double>()}, true}
+		{
+		}
+
+		virtual Data operator()(const std::vector<Data>&) const override
+		{
+			return "blah";
+		}
+};
+
+class TwoDouble : public Overload
+{
+	public:
+		TwoDouble():
+			Overload{{SignatureType::create<double>(), SignatureType::create<double>()}, false}
+		{
+		}
+
+		virtual Data operator()(const std::vector<Data>&) const override
+		{
+			return "blah";
+		}
+};
+
+class DataTestFixture
+{
+	public:
+		/// Constructor.
+		DataTestFixture():
+			// A normal overload, and two possibly ambiguous overloads
+			function{{std::make_shared<OneInt>(), std::make_shared<VariadicDouble>(), std::make_shared<TwoDouble>()}}
+		{
+		}
+
+		Function function;
+};
+
+BOOST_FIXTURE_TEST_SUITE(DataTest, DataTestFixture)
+
+BOOST_AUTO_TEST_CASE(callOperatorNormal)
+{
+	BOOST_TEST(function({3}).get<std::string>() == "blah");
+}
+
+BOOST_AUTO_TEST_CASE(callOperatorAmbiguous)
+{
+	BOOST_CHECK_THROW(function({2.2}), ScriptError);
+}
+
+BOOST_AUTO_TEST_CASE(callOperatorNoOverloadFound)
+{
+	BOOST_CHECK_THROW(function({2, "bluh"}), ScriptError);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
